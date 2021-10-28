63a431554226016d7e5b7770d2c81afecbd9eb21
SNValadez <snvaladez13@gmail.com>
2021-10-28 10:45:11 -0700

    CREATE TABLE employee in schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -1,11 +1,19 @@
 CREATE TABLE department (
-    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    id INTEGER NOT NULL AUTO_INCREMENT,
     name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE role (
-    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    id INTEGER NOT NULL AUTO_INCREMENT,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
     department_id INTEGER
+);
+
+CREATE TABLE employee (
+    id INTEGER NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INTEGER
 );
