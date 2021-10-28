0d06b518df8ca344cfe9a4a0259c68ff97cccccf
SNValadez <snvaladez13@gmail.com>
2021-10-28 12:01:41 -0700

    additions to table

--- db/schema.sql
+++ db/schema.sql
@@ -1,19 +1,31 @@
 CREATE TABLE department (
-    id INTEGER NOT NULL AUTO_INCREMENT,
+    id INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT,
     name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE role (
-    id INTEGER NOT NULL AUTO_INCREMENT,
+    id INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
     department_id INTEGER
+    FOREIGN KEY (department_id)
+        REFERENCES role (id)
+        ON DELETE SET NULL
+        ON UPDATE CASCADE
 );
 
 CREATE TABLE employee (
-    id INTEGER NOT NULL AUTO_INCREMENT,
+    id INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_id INT NOT NULL,
+    role_id INT NOT NULL
+    FOREIGN KEY (role_id)
+        REFERENCES employee (id)
+        ON DELETE SET NULL
+        ON UPDATE CASCADE,
     manager_id INTEGER
+    FOREIGN KEY (manager_id)
+        REFERENCES employee (id)
+        ON DELETE SET NULL
+        ON UPDATE CASCADE 
 );
--- server.js
+++ server.js
@@ -1,3 +1,15 @@
 const mysql = require("mysql2");
 const inquirer = require("inquirer");
 const cTable = require("console.table");
+
+// let array = [{
+//     name: 'foo',
+//     age: 10
+//   }, {
+//     name: 'bar',
+//     age: 20
+//   }];
+// console.log(array);
+// console.table(array);
+// console.warn();
+// console.error();
