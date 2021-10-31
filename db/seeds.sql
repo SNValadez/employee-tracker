efa6ef475cf9572b13d3b76b995646623cae87fa
SNValadez <snvaladez13@gmail.com>
2021-10-31 16:41:42 -0700

    add values to role

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,11 +1,20 @@
 USE employees;
 
-INSERT INTO department (name) VALUES 
+INSERT INTO department(name) VALUES 
     ("Books"), 
     ("Shoes"),
     ("Legal"),
     ("Toys");
 
-INSERT INTO role(title, salary, department_id)
+INSERT INTO role(title, salary, department_id) VALUES
+    ("Book Sorter", 18000, 1),
+    ("Shoe Stocker", 20000, 2),
+    ("Lawyer", 80000, 3),
+    ("Toy Maker", 60000, 4);
+
+INSERT INTO employee(first_name, last_name, role_id, manager_id) VALUES
+    ("Joseph", ""),
+    ()
+
 
 
