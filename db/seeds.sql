46b13aecb431d9857ce8097fcf1dfd5a1bd10854
SNValadez <snvaladez13@gmail.com>
2021-10-31 17:00:15 -0700

    added values to employee

--- db/seeds.sql
+++ db/seeds.sql
@@ -13,8 +13,11 @@ INSERT INTO role(title, salary, department_id) VALUES
     ("Toy Maker", 60000, 4);
 
 INSERT INTO employee(first_name, last_name, role_id, manager_id) VALUES
-    ("Joseph", ""),
-    ()
+    ("Joseph", "Campbell", 3, null),
+    ("Michael", "Cromwell", 2, 2),
+    ("Cheryl", "Kennedy", 4, null),
+    ("Faherty", "Raines", 3, 2),
+    ("Ghillie", "Herde", 1, null);
 
 
 
